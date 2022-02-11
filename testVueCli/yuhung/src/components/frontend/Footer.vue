e4a24943bee08da1c11a51d1b734a23f54f81412
hung <ff789456741@gmail.com>
2022-02-11 14:08:28 +0800

    修改 footer

--- testVueCli/yuhung/src/components/frontend/Footer.vue
+++ testVueCli/yuhung/src/components/frontend/Footer.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="main-footer">
-      <pre class="conpyright">
+      <p class="conpyright">
         Copyright &copy; 2021 All Rights Reserved.
-        僅個人作品練習，無商業用途
-      </pre>
+      </p>
+      僅個人作品練習，無商業用途
     </div>
   </div>
 </template>
