4e7dd09aa5b959f092b8e8d14c192535873cc6e6
hung <ff789456741@gmail.com>
2022-02-11 14:11:20 +0800

    刪除.nav

--- testVueCli/yuhung/src/views/frontend/Home.vue
+++ testVueCli/yuhung/src/views/frontend/Home.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="wrap">
     <div class="home">
-      <div class="nav"></div>
-      <Banner/>
+      <BattleRoyale/>
       <div class="banner">
-        <img src="../../assets/image/banner.jpg" alt="banner">
+        <img src="../../assets/image/Arenas.jpg" alt="banner">
       </div>
     </div>
   </div>
 </template>
 
 <script>  
-import Banner from '../../components/frontend/Banner.vue'
+import BattleRoyale from '../../components/frontend/BattleRoyale.vue'
 
 export default {
   name: 'Home',
   components: {
-    Banner,
+    BattleRoyale,
   }
 }
 </script>
@@ -24,13 +23,10 @@ export default {
 <style lang="scss" scoped>
   .wrap {
     margin: auto;
-    background: url('../../assets/image/banner.jpg') no-repeat center center / cover;
+    background: url('../../assets/image/Arenas.jpg') no-repeat center center / cover;
     background-color: rgba(0, 0, 0, .6);
     background-blend-mode: multiply;
     background-attachment: fixed;
-  }
-  .nav {
-    min-height: 85px;
   }
   .banner {
     max-width: 1890px;
