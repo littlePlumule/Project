d1fa84f9882e184a967ac5698449fabfb16a6d9f
hung <ff789456741@gmail.com>
2022-02-11 14:08:44 +0800

    修改 Navbar

--- testVueCli/yuhung/src/components/frontend/Navbar.vue
+++ testVueCli/yuhung/src/components/frontend/Navbar.vue
@@ -1,49 +1,57 @@
 <template>
   <div>
     <input type="checkbox" id="navbar__control">
-    <header class="main-header">
-      <div class="container">
-        <router-link to="/" class="logo">
-          <img src="../../assets/image/logo.png" alt="apex logo">
-        </router-link>
-        <div class="wrap">
-          <nav class="main-nav">
-            <router-link to="/about">
-              <span>關於</span>
-            </router-link>
-            <router-link to="/products">
-              <span>活動</span>
-            </router-link>
-            <router-link to="/products">
-              <span>商店</span>
-            </router-link>
-            <router-link to="/products">
-              <span>訂單</span>
-            </router-link>
-          </nav>
-          <ul class="list">
-            <li>
-              <a href="#">
-                <i class="far fa-heart"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="fas fa-shopping-cart"></i>
-              </a>
-            </li>
-            <li>
-              <router-link to="/login">
-                <i class="fas fa-user"></i>
+    <div class="height">
+      <header class="main-header">
+        <div class="container">
+          <router-link to="/" class="logo">
+            <img src="../../assets/image/logo.png" alt="apex logo">
+          </router-link>
+          <div class="wrap">
+            <nav class="main-nav">
+              <router-link to="/about/legends">
+                <span>關於</span>
               </router-link>
-            </li>
-          </ul>
+              <router-link to="/products">
+                <span>活動</span>
+              </router-link>
+              <router-link to="/products">
+                <span>商店</span>
+              </router-link>
+              <router-link to="/products">
+                <span>訂單</span>
+              </router-link>
+            </nav>
+            <ul class="list">
+              <li>
+                <a href="#">
+                  <i class="far fa-heart"></i>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fas fa-shopping-cart"></i>
+                </a>
+              </li>
+              <li>
+                <router-link to="/login">
+                  <i class="fas fa-user"></i>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <label class="menu__btn" for="navbar__control"></label>
         </div>
-        <label class="menu__btn" for="navbar__control"></label>
-      </div>
-    </header>
+      </header>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
 
 <style lang="scss" scoped src="../../assets/scss/UserNavbar.scss">
 </style>
