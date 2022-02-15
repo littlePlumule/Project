980c95dfaa07d076e736c447a462221b47f8076e
hung <ff789456741@gmail.com>
2022-02-15 12:19:11 +0800

    新增活動頁面

---
+++ testVueCli/yuhung/src/views/frontend/Activity.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="banner">
+      <div class="coupon">快來領取喔</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .banner {
+    max-width: 1890%;
+    background: url('../../assets/image/test.png') no-repeat center center / cover;
+    padding: 5em;
+  }
+  .coupon {
+    text-align: center;
+    font-weight: bold;
+    font-size: 48px;
+    padding: 3rem 2rem;
+    background-color: rgba(208, 109, 109, .8);
+    color: #fff;
+    border-radius: 10px;
+  }
+</style>
