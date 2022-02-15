2991ad0d83da3220383e8b13a4bbd16040016290
hung <ff789456741@gmail.com>
2022-02-15 13:32:56 +0800

    新增首頁 router

--- testVueCli/shop/src/router/index.js
+++ testVueCli/shop/src/router/index.js
@@ -1,23 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '*',
+    redirect: '/',
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    // name: 'Layout',
+    component: () => import('../components/Layout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+      },
+      // {
+      //   path: 'productslist',
+      //   name: 'ProductsList',
+      //   component: ProductsList,
+      // },
+      // {
+      //   path: 'productslist/:productId',
+      //   name: 'Product',
+      //   component: Product,
+      // },
+      // {
+      //   path: 'coupongame',
+      //   name: 'CouponGame',
+      //   component: CouponGame,
+      // },
+      // {
+      //   path: 'createorder',
+      //   name: 'CreateOrder',
+      //   component: CreateOrder,
+      // },
+    ]
+  },
 ]
 
 const router = new VueRouter({
