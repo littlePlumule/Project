e941a826e5a65a12c3a0ab42b9fdad20b435b153
hung <ff789456741@gmail.com>
2022-02-15 13:32:36 +0800

    新增導覽列

--- testVueCli/shop/src/components/Navbar.vue
+++ testVueCli/shop/src/components/Navbar.vue
@@ -1,17 +1,54 @@
 <template>
   <div>
     <div class="wrap">
-      <div class="logo"></div>
-      <nav class="nav"></nav>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+      <div class="container">
+        <div class="logo">yuhung's Shop</div>
+        <nav class="nav">
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+          <a href="#">Coupons</a>
+        </nav>
+      </div>
+      <ul class="icon">
+        <li><i class="fas fa-user-circle"></i></li>
+        <li><i class="fas fa-shopping-cart"></i></li>
+        <li><i class="fas fa-heart"></i></li>
       </ul>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  
+  .wrap {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1140px;
+    margin: auto;
+    padding: 1em;
+    .container {
+      display: flex;
+      align-items: center;
+      .logo {
+        font-size: 1.25rem;
+        font-weight: bold;
+      }
+      .nav {
+        a {
+          text-decoration: none;
+          color: rgba(0,0,0,.5);
+          margin-left: 24px;
+          height: 100%;
+        }
+      }
+    }
+    
+    .icon {
+      display: flex;
+      font-size: 20px;
+      i {
+        margin-right: 1em;
+      }
+    }
+  }
 </style>
